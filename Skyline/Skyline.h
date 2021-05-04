d068ac4caf218fc1157fb1feb37aadbb948fd18a
Жохов Владислав Владимирович <Zhokhov.V.V@sberbank.ru>
2021-05-04 19:01:02 +0300

    Initial commit

---
+++ Skyline/Skyline.h
@@ -0,0 +1,18 @@
+//
+//  Skyline.h
+//  Skyline
+//
+//  Created by 17790204 on 04.05.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Skyline.
+FOUNDATION_EXPORT double SkylineVersionNumber;
+
+//! Project version string for Skyline.
+FOUNDATION_EXPORT const unsigned char SkylineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Skyline/PublicHeader.h>
+
+
